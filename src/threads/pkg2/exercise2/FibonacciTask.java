5cf5bb003339e9b22cd3175e6b232a91608e215b
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-08-26 13:46:26 +0200

    Exercise completed

--- src/threads/pkg2/exercise2/FibonacciTask.java
+++ src/threads/pkg2/exercise2/FibonacciTask.java
@@ -1,26 +1,34 @@
 package threads.pkg2.exercise2;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class FibonacciTask extends Thread{
+public class FibonacciTask extends Thread {
+
     private long tal;
     List<FibonacciObserver> observers = new ArrayList();
-    
-    public void registerFibonacciObserver(FibonacciObserver o){
-      observers.add(o);
+
+    public void registerFibonacciObserver(FibonacciObserver o) {
+        observers.add(o);
     }
-    
+
     public FibonacciTask(long n) {
         this.tal = n;
     }
+
     @Override
     public void run() {
-        //Call the Fibonacci method from here
-        //long tal = ......
-        for(FibonacciObserver observer : observers){
-          observer.dataReady(tal);
+        tal = fib(tal);
+        for (FibonacciObserver observer : observers) {
+            observer.dataReady(tal);
+        }
+    }
+
+    private long fib(long n) {
+        if ((n == 0) || (n == 1)) {
+            return n;
+        } else {
+            return fib(n - 1) + fib(n - 2);
         }
     }
 }
